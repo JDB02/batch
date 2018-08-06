2f45e879414f6c8f42df3daddd3bfad43d9805de
JDB02 <joshbrown02@hotmail.com>
2018-08-05 22:15:32 -0400

    Create cybernews.bat
    Very simple script to open cybersecurity news providers

---
+++ cybernews.bat
@@ -0,0 +1,8 @@
+@echo off
+start "" https://www.fifthdomain.com/
+start "" https://www.darkreading.com/
+start "" https://thehackernews.com/
+start "" https://xato.net/
+start "" https://www.technewsworld.com/perl/section/cyber-security
+start "" https://www.fireeye.com/blog.html
+start "" https://www.securityweek.com/
